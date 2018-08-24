311135342d74797173950bb62b2f8f538606da46
Ana Cristina Torres Vargas <cristi_torres94@hotmail.com>
2018-08-23 21:50:50 -0500

    create deashboard stack

--- src/components/techStack/TechStack.js
+++ src/components/techStack/TechStack.js
@@ -6,7 +6,7 @@ import htmlLogo from "./../../assets/images/stack/html5.png";
 import cssLogo from "./../../assets/images/stack/css.png";
 import jsLogo from "./../../assets/images/stack/js-logo.png";
 import materializeLogo from "./../../assets/images/stack/materialize.jpg";
-import reactD3 from "./../../assets/images/stack/react-d3.jpg";
+import reactD3 from "./../../assets/images/stack/react-d3.png";
 import primereact from "./../../assets/images/stack/primereact.png";
 import docker from "./../../assets/images/stack/docker.png";
 import github from "./../../assets/images/stack/github.png";
@@ -15,8 +15,8 @@ import github from "./../../assets/images/stack/github.png";
 const techStack = (props) => (
              <div className='center'>
                 <Row>
-                    <Col s={1} m={2} l={2} className='stack-title'><div>Web</div></Col>
-                    <Col s={10} m={10} l={10}>
+                    <Col s={12} m={12} l={2} className='stack-title'><div>Web</div></Col>
+                    <Col s={12} m={12} l={10} className='logos'>
                         <div className='stack-logo'><img src={reactLogo} ></img></div>
                         <div className='stack-logo'><img src={htmlLogo} ></img></div>
                         <div className='stack-logo'><img src={cssLogo} ></img></div>
@@ -27,14 +27,14 @@ const techStack = (props) => (
                     </Col>
                 </Row>
                 <Row>
-                    <Col s={1} m={2} l={2}><div>Infra</div></Col>
-                    <Col s={10} m={10} l={10}>
+                    <Col s={12} m={12} l={2} className='stack-title2 '><div>Infra</div></Col>
+                    <Col s={12} m={12} l={10} className='logos light'>
                         <div className='stack-logo'><img src={docker}></img></div>
                     </Col>
                 </Row>
                 <Row>
-                    <Col s={1} m={2} l={2} className='stack-title'><div>Collaboration</div></Col>
-                    <Col s={10} m={10} l={10}>
+                    <Col s={12} m={12} l={2} className='stack-title'><div>Collaboration</div></Col>
+                    <Col s={12} m={12} l={10} className='logos'>
                         <div className='stack-logo'><img src={github}></img></div>
                     </Col>
                 </Row>
