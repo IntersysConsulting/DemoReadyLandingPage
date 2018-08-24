9a0f1cc9d5ea83cd151153cc9e495ca58f9db93c
nestorivan <nestor.lopez.delgadillo@gmail.com>
2018-08-23 22:04:07 -0500

    add code refactor

--- src/components/partners/Partnership.js
+++ src/components/partners/Partnership.js
@@ -1,46 +1,49 @@
-import React from 'react'
+import React from "react";
 
-import aws from "./../../assets/images/logo_aws.jpg";
-import elastic from "./../../assets/images/logo_elastic.jpg";
-import microsoft from "./../../assets/images/logo_microsoft.png";
-import cloudera from "./../../assets/images/logo_cloudera.jpg";
-import hortonworks from "./../../assets/images/logo_hortonworks.jpg";
-import mapr from "./../../assets/images/logo_mapr.jpg";
+import "./Partnership.css";
+const partners = [
+  {
+    name: "Amazon Web Services",
+    img: "aws",
+    ext: "jpg"
+  },
+  {
+    name: "Elastic",
+    img: "elastic",
+    ext: "jpg"
+  },
+  {
+    name: "Gold Microsoft Partner ",
+    img: "microsoft",
+    ext: "png"
+  },
+  {
+    name: "Cloudera",
+    img: "cloudera",
+    ext: "jpg"
+  },
+  {
+    name: "Horton Works",
+    img: "hortonworks",
+    ext: "jpg"
+  },
+  {
+    name: "Mapr",
+    img: "mapr",
+    ext: "jpg"
+  }
+];
 
-import './Partnership.css'
-
-export default() => {
-    const partners = [
-        {
-            name: "Amazon Web Services",
-            img: aws
-        }, {
-            name: "Elastic",
-            img: elastic
-        }, {
-            name: "Gold Microsoft Partner ",
-            img: microsoft
-        }, {
-            name: "Cloudera",
-            img: cloudera
-        }, {
-            name: "Horton Works",
-            img: hortonworks
-        }, {
-            name: "Mapr",
-            img: mapr
-        }
-    ];
-    return (
-        <div className="partners row">
-            {partners.map(partner => (
-                <div key={partner.name}
-                className="partners__container col s6 m2 center-align">
-                    <img className="partners__logo"
-                    src={partner.img} alt={partner.name}/>
-                </div>
-            ))
-}
-        </div>
-    )
-}
+export default () => (
+  <div className="partners row">
+    {partners.map(({ name, img, ext }) => (
+      <div key={name} className="partners__container col s6 m2 center-align">
+        <img
+          className="partners__logo"
+          src={`/img/partners/logo_${img}.${ext}`}
+          alt={name}
+        />
+      </div>
+    ))}
+  </div>
+);
