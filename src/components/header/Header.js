37febc6d546be256f85cf7e552ee98761845b4af
Nestor Lopez <nlopez@intersysconsulting.com>
2018-08-28 12:42:24 -0500

    add code refactor

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,15 +3,13 @@ import { Navbar, NavItem, Icon } from 'react-materialize'
 import './Header.css';
 import logo from '../../assets/images/intersys-logo.png';
 
-const header = (props) => (
-    <div>
+const header = () => (
         <Navbar brand={<img href="/" alt="navbar logo" className="navbar_logo" src={logo} />} className='navbar_intersys' right>
             <NavItem href='#call-to-action'><Icon>home</Icon></NavItem>
             <NavItem href='#demos'>Demos</NavItem>
             <NavItem href='#partner'>Partnership</NavItem>
             <NavItem href='/'>About Us</NavItem>
         </Navbar>
-    </div>
 );
 
 export default header;
