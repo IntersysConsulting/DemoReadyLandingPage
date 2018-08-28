364e8e80bbc23e46758da5c4009b28ca98c7f58c
Nestor Lopez <nlopez@intersysconsulting.com>
2018-08-27 20:00:31 -0500

    modify gitignore add categories component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import Header from './components/header/Header';
 import CallToAction from './components/calltoaction/CallToAction';
 import Partnership from './components/partners/Partnership';
 import Footer from './components/footer/footer'
-import Services from "./components/containers/services/services"
+import Categories from "./components/containers/categories/categories"
 
 const App = () => (
     <div>
       <Header></Header>
       <CallToAction></CallToAction>
-      <Services/>
+      <Categories/>
       <footer id="partner">
         <Partnership/>
         <Footer/>
       </footer>
-    </div>    
+    </div>
   );
 
 export default App;
