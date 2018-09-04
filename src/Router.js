45bcb44f2ab734365a088718f05cb2d6103cd862
david-intersys <drodriguez@intersysconsulting.com>
2018-09-04 15:56:53 -0500

    Component for 404 error

--- src/Router.js
+++ src/Router.js
@@ -3,12 +3,14 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import DemoPage from './components/demoPage/DemoPage';
 import App from './App';
+import NotFound from './components/notFound/NotFound';
 
 const router = (props) =>(
           <BrowserRouter>
             <Switch>
               <Route exact path='/' component={App} />
               <Route path='/demo-page/:category/:demoId/:component' component={DemoPage} />
+              <Route component={NotFound} />
             </Switch>
           </BrowserRouter>
         );
