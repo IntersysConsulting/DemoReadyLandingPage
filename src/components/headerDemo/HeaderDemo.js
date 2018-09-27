c543229d8ca72ed879aec7cd6e4f06a1e514b3be
david-intersys <drodriguez@intersysconsulting.com>
2018-09-27 17:26:05 -0500

    Fix for paths and code refactor

--- src/components/headerDemo/HeaderDemo.js
+++ src/components/headerDemo/HeaderDemo.js
@@ -10,36 +10,38 @@ class HeaderDemo extends Component {
     cleanUrl = () => {
         const {category, demoId} = this.props.match.params;
 
-        return addRepoName(`demo-page/${category}/${demoId}`);
+        return addRepoName(`/demo-page/${category}/${demoId}`);
     }
 
     render() {
+        const home = addRepoName("/");
+        const url_clean = this.cleanUrl();
         return [
+            
         <div className = "header" > <Navbar
-            brand={< img alt="navbar_logo" href={addRepoName("/")} className="navbar_logo" src = {
-            logo
-        } />}
+            brand={< img alt="navbar_logo" className="navbar_logo" src = {logo} />}
+            href={home}
             className='navbar_intersys'
             right>
-            <NavItem className="home-icon" href={addRepoName('/')}>
+            <NavItem className="home-icon" href={home}>
                 <Icon>home</Icon>
             </NavItem>
-            <NavItem className="study" href={'/' + this.cleanUrl() + '/case'}>
+            <NavItem className="study" href={url_clean + '/case'}>
                 <span>Case</span>
                 <Icon className="study-icon"></Icon>
             </NavItem>
             <NavItem
                 className="demo-video"
-                href={'/' + this.cleanUrl() + '/demo'}
+                href={url_clean + '/demo'}
                 src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0">
                 <span>Demo</span>
                 <Icon className="demo-icon"></Icon>
             </NavItem>
-            <NavItem className="credits" href={'/' + this.cleanUrl() + '/credits'}>
+            <NavItem className="credits" href={url_clean + '/credits'}>
                 <span>Credits</span>
                 <Icon className="credits-icon"></Icon>
             </NavItem>
-            <NavItem className="stack" href={'/' + this.cleanUrl() + '/tech'}>
+            <NavItem className="stack" href={url_clean + '/tech'}>
                 <span>Tech Stack</span>
                 <Icon className="stack-icon"></Icon>
             </NavItem>
